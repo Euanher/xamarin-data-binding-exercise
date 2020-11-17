9e0e21717414b03a24fd24f0fad0c1fce1ce85f2
Euanher <70320894+Euanher@users.noreply.github.com>
2020-11-17 15:20:43 +0200

    Xaml
    exercise 1

--- src/exercise1/start/FlagFacts/Extensions/FlagExtensions.cs
+++ src/exercise1/start/FlagFacts/Extensions/FlagExtensions.cs
@@ -18,4 +18,19 @@ namespace FlagFacts.Extensions
             return ImageSource.FromResource(flag.ImageUrl, flag.GetType().GetTypeInfo().Assembly);
         }
     }
+    public class EmbeddedImageConverter : IvalueConverter
+    {
+    public Type ResolvingAssemblyType {get; set; }
+    
+    public object Convert(object value. Type targetType, object parameter, CultureInfo culture)
+    {
+    var imageUrl = (value ?? " ").ToString();
+    if (string.IsNullOrEmpty(imageUrl))
+    return null;
+    return ImageSource.FromResource(imageUrl, ResolvingAssemblyType?.GetTypeInfo().Assembly);
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+    throw new NotSupportedException($"{nameof(EmbeddedImageConverter)} cannot be used on two-way bindings.");
+    }
 }
